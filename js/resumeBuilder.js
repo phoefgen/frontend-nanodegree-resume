5ac8ffd2f98109b051c80902be3f5a7b97d384ef
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2017-07-31 06:52:24 -0700

    sync

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,10 @@
 // Helper functions
-var hasContent = function (object) {
-  // Determine if an object is empty. 
-  if (object.length > 0) {
-    return true
-  }
-  return false
+var hasContent = function(object) {
+    // Determine if an object is empty. 
+    if (object.length > 0) {
+        return true
+    }
+    return false
 }
 
 //work
@@ -12,42 +12,49 @@ var hasContent = function (object) {
 // an employer, title, location, dates worked and description strings.
 
 var work = {
-  "jobs": [{
-      "employer": "Demonware",
-      "title": "Service Reliability Engineer",
-      "location": "Vancouver BC",
-      "dates": "May 2014-present.",
-      "description": "Keeping Activision Video games running"
-    },
-    {
-      "employer": "Radiant Communications",
-      "title": "Corporate Network support",
-      "location": "Vancouver BC",
-      "description": "Keeping the coffee flowing.",
-      "dates": "August 2013-May 2014"
-    },
-    {
-      "employer": "iiNet",
-      "title": "Network Administrator",
-      "location": "Perth Australia",
-      "dates": "May 2011-June 2013",
-      "description": "Keeping the internet running"
-    }
-  ]
+    "jobs": [{
+            "employer": "Activision (Demonware) - Monitoring Team.",
+            "title": "Service Reliability Engineer",
+            "location": "Vancouver BC",
+            "dates": "March 2017-present.",
+            "description": "Building tooling, process, and teams to automate modern monitoring services. Developed new tooling, proposed and implemented process that has directly contributed to a 10x decrease in the number of actionable alerts. System design, and peer review of others system design and architecture. Lead product manager on a new observability platform based around data driven automation.",
+        },
+        {
+            "employer": "Activision (Demonware) - NOC",
+            "title": "Linux Sys Admin",
+            "location": "Vancouver BC, Canada",
+            "description": "Ensuring service quality for Call of Duty, Destiny, Diablo, Guitar Hero etc. Performed monitoring, break-fix and emergency management duties for over 7000 hosts",
+            "dates": "May 2014-March 2017"
+        },
+        {
+            "employer": " Radiant Communications",
+            "title": "Support",
+            "location": "Vancouver, BC, Canada",
+            "dates": "August 2013-May 2014",
+            "description": "Provide corporate network support for secure payment services. Technical Escalation point for last mile services (Fibre, SHDSL, MPLS/VPN, Juniper Routers)"
+        },
+        {
+            "employer": "iiNet",
+            "title": "Network Administrator.",
+            "location": "Perth, BC, Australia.",
+            "dates": "August 2013-May 2014",
+            "description": "Detect, triage, and resolve all network and server issues at a National Australian ISP. Monitored and performed break-fix actions on all tech infrastructure (Servers and Network)"
+        }
+    ]
 };
 
-work.display = function () {
-  // Display only if jobs exist
-  if (hasContent(work.jobs)) {
-    $("#workExperience").prepend(HTMLworkStart)
-    work.jobs.forEach(function (job) {
-      $("#workExperience").append(HTMLworkEmployer.replace("%data%", job.employer));
-      $("#workExperience").append(HTMLworkTitle.replace("%data%", job.title));
-      $("#workExperience").append(HTMLworkDates.replace("%data%", job.dates));
-      $("#workExperience").append(HTMLworkLocation.replace("%data%", job.location));
-      $("#workExperience").append(HTMLworkDescription.replace("%data%", job.description));
-    })
-  }
+work.display = function() {
+    // Display only if jobs exist
+    if (hasContent(work.jobs)) {
+        $("#workExperience").prepend(HTMLworkStart)
+        work.jobs.forEach(function(job) {
+            $("#workExperience").append(HTMLworkEmployer.replace("%data%", job.employer));
+            $("#workExperience").append(HTMLworkTitle.replace("%data%", job.title));
+            $("#workExperience").append(HTMLworkDates.replace("%data%", job.dates));
+            $("#workExperience").append(HTMLworkLocation.replace("%data%", job.location));
+            $("#workExperience").append(HTMLworkDescription.replace("%data%", job.description));
+        })
+    }
 }
 work.display()
 
@@ -57,50 +64,48 @@ work.display()
 // URL strings for project images.
 
 var projects = {
-  "projects": [{
-      "title": "Design and Deployed new Monitoring System",
-      "dates": "Feb 2016 to present.",
-      "description": "Building an automation suite for Demonware monitoring.",
-      "images": [
-        "https://unsplash.it/200?image=10"
-      ]
-    },
-    {
-      "title": "Built exo-suit",
-      "dates": "Feb 2016-May 2015",
-      "description": "Built a flying armoured suit.",
-      "images": ["https://unsplash.it/200?image=14",
-        "https://unsplash.it/200?image=11"
-      ]
-    },
-    {
-      "title": "Exterminated all Rats in New York.",
-      "dates": "July 2009-May 2014",
-      "description": "It was a big job, but somebody had to do it.",
-      "images": [
-        "https://unsplash.it/200?image=12"
-      ]
-    }
-  ]
+    "projects": [{
+            "title": "Product manager on monitoring modernisation project for Activision online titles",
+            "dates": "Feb 2016 to present.",
+            "description": "Managing the transition from 7000+ hosts on Nagios with  Tier1 NOC technicians performing triage  to automated monitoring  pipelines based around Sensu and Stackstorm automation.Implemented Agile process, developed and delivered team upskilling and training, actively contributed code, and driving formal requirements gathering.",
+            "images": [
+                "images/demonware_logo1.png"
+            ]
+        },
+        {
+            "title": "",
+            "dates": "Feb 2016-May 2015",
+            "description": "Lead the design, build, support in production of an experimental, monitoring system using Sensu, docker, jenkins, stackstorm, and AWS. Prototype now being extended by a full team into a platform to support Call of Duty in 2017",
+            "images": ["images/demonware_logo1.png"]
+        },
+        {
+            "title": "Built a suite of custom monitoring tooling",
+            "dates": "July 2009-May 2014",
+            "description": "Including writing software to federate 8 monitoring service dashboards into a single pane of glass.",
+            "images": [
+                "images/demonware_logo1.png"
+            ]
+        }
+    ]
 };
 
-projects.display = function () {
-  // Display projects if they exist.
-  if (hasContent(projects.projects)) {
-    $("#projects:first").append(HTMLprojectStart)
+projects.display = function() {
+    // Display projects if they exist.
+    if (hasContent(projects.projects)) {
+        $("#projects:first").append(HTMLprojectStart)
 
-    // Generate project detail:
-    projects.projects.forEach(function (project) {
-      $("#projects:last").append(HTMLprojectTitle.replace("%data%", project.title));
-      $("#projects:last").append(HTMLprojectDates.replace("%data%", project.dates));
-      $("#projects:last").append(HTMLprojectDescription.replace("%data%", project.description))
+        // Generate project detail:
+        projects.projects.forEach(function(project) {
+            $("#projects:last").append(HTMLprojectTitle.replace("%data%", project.title));
+            $("#projects:last").append(HTMLprojectDates.replace("%data%", project.dates));
+            $("#projects:last").append(HTMLprojectDescription.replace("%data%", project.description))
 
-      // Support N project images. 
-      project.images.forEach(function (imageUrl) {
-        $("#projects:first").append(HTMLprojectImage.replace("%data%", imageUrl))
-      })
-    })
-  }
+            // Support N project images. 
+            project.images.forEach(function(imageUrl) {
+                $("#projects:first").append(HTMLprojectImage.replace("%data%", imageUrl))
+            })
+        })
+    }
 }
 projects.display()
 
@@ -112,44 +117,49 @@ projects.display()
 // The twitter property is optional.
 
 var bio = {
-  "name": "Paul Hoefgen",
-  "role": "Automation Engineer",
-  "welcomeMessage": "Welcome to a webpage with stuff on it.",
-  "bioPic": "images/me_jeep.jpg",
-  "contacts": {
-    "mobile": "+1 204 228 0683",
-    "email": "paul.hoefgen@gmail.com",
-    "twitter": "@singlelement",
-    "github": "phoefgen",
-    "location": "Vancouver, BC"
-  },
-  "skills": ["eating",
-    "sleeping",
-    "breakfast"
-  ],
+    "name": "Paul Hoefgen",
+    "role": "Automation Engineer",
+    "welcomeMessage": "Observability and process automation engineer.",
+    "bioPic": "images/me_jeep.jpg",
+    "contacts": {
+        "mobile": "+1 204 228 0683",
+        "email": "paul.hoefgen@gmail.com",
+        "twitter": "@singlelement",
+        "github": "github.com/phoefgen",
+        "location": "Vancouver, BC"
+    },
+    "skills": ["Python",
+        "Flask",
+        "Sensu",
+        "Nagios",
+        "Stackstorm",
+        "Docker",
+        "Product Mangagement",
+        "Linux",
+        "Process Automation",
+        "System Design"
+    ],
 };
 
-bio.display = function () {
-  // Display static data:
-  $("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));
-  $("#header:last").append(HTMLheaderName.replace("%data%", bio.name));
-  $("#header:last").append(HTMLheaderRole.replace("%data%", bio.role));
+bio.display = function() {
+    // Display static data:
+    $("#headShot").append(HTMLbioPic.replace("%data%", bio.bioPic));
+    $("#bannerData:last").append(HTMLheaderName.replace("%data%", bio.name));
+    $("#bannerData:last").append(HTMLheaderRole.replace("%data%", bio.role));
 
-  // Build top contacts string:
-  var contactList = bio.contacts.twitter + ' ' +
-    bio.contacts.location + ' ' +
-    'Github: ' +
-    bio.contacts.github
-  $("#header:last").append(contactList)
-  $("#header:last").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+    // Build top contacts string:
+    var contactList = bio.contacts.location + ' twitter: ' + bio.contacts.twitter +
+        ' Github: ' + bio.contacts.github
+    $("#bannerData:last").append(contactList)
+    $("#header:last").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 
-  // Display additional data:
-  if (hasContent(bio.skills)) {
-    $("#header:last").append(HTMLskillsStart);
-    bio.skills.forEach(function (skill) {
-      $("#header:last").append(HTMLskills.replace("%data%", skill));
-    })
-  }
+    // Display additional data:
+    if (hasContent(bio.skills)) {
+        $("#header:last").append(HTMLskillsStart);
+        bio.skills.forEach(function(skill) {
+            $("#header:last").append(HTMLskills.replace("%data%", skill));
+        })
+    }
 }
 bio.display();
 
@@ -161,68 +171,57 @@ bio.display();
 // strings.
 
 var education = {
-  "schools": [{
-      "name": "Trump University",
-      "location": "New York",
-      "degree": "Handicrafts",
-      "majors": ["Womens Studies"],
-      "dates": "May6th2015-May12th2015",
-      "url": "http://trumpuniversity.com/"
-    },
-    {
-      "name": "Trumpet College",
-      "location": "Boston",
-      "degree": "Closet Botany",
-      "majors": ["Eating Steak",
-        "Advanced Womens Studies"
-      ],
-      "dates": "May9th2016-May10th2016",
-      "url": "http://trumpetcollege.com/"
-    }
-  ],
-  "onlineCourses": [{
-      "title": "Full Stack Nanodegree",
-      "school": "Udacity",
-      "dates": "May9th2016-May10th201",
-      "url": "udacity.com"
-    },
-    {
-      "title": "Front End Nanodegree",
-      "school": "Udacity",
-      "dates": "May9th2017-May10th2018",
-      "url": "udacity.com"
-    }
-  ]
+    "schools": [{
+        "name": "Royal Melbourne Institue of Technology",
+        "location": "New York",
+        "degree": "Handicrafts",
+        "majors": ["Computer Science"],
+        "dates": "May 6th 2015 - May 12th 2015",
+        "url": "http://rmit.edu.au/"
+    }],
+    "onlineCourses": [{
+            "title": "Full Stack Nanodegree",
+            "school": "Udacity",
+            "dates": "Febuary 2016 - August 2017",
+            "url": "udacity.com"
+        },
+        {
+            "title": "Front End Nanodegree",
+            "school": "Udacity",
+            "dates": "Feburary 2017 - Ongoing",
+            "url": "udacity.com"
+        }
+    ]
 }
 
-education.display = function () {
-  if (hasContent(education.schools)) {
-    $("#education:first").append(HTMLschoolStart);
+education.display = function() {
+    if (hasContent(education.schools)) {
+        $("#education:first").append(HTMLschoolStart);
 
-    education.schools.forEach(function (school) {
-        $("#education:last").append(HTMLschoolName.replace("%data%", school.name));
-        $("#education:last").append(HTMLschoolDates.replace("%data%", school.dates));
-        $("#education:last").append(HTMLschoolLocation.replace("%data%", school.location));
-        $("#education:last").append(HTMLschoolMajor.replace("%data%", school.degree));
-        school.majors.forEach(function (major) {
-          $("#education:last").append(HTMLschoolDegree.replace("%data%", major))
+        education.schools.forEach(function(school) {
+            $("#education:last").append(HTMLschoolName.replace("%data%", school.name));
+            $("#education:last").append(HTMLschoolDates.replace("%data%", school.dates));
+            $("#education:last").append(HTMLschoolLocation.replace("%data%", school.location));
+            $("#education:last").append(HTMLschoolMajor.replace("%data%", school.degree));
+            school.majors.forEach(function(major) {
+                $("#education:last").append(HTMLschoolDegree.replace("%data%", major))
+            })
         })
-      })
     }
 
-if (hasContent(education.onlineCourses)) {
-  $("#education:last").append(HTMLonlineClasses);
+    if (hasContent(education.onlineCourses)) {
+        $("#education:last").append(HTMLonlineClasses);
 
-  education.onlineCourses.forEach(function (course) {
-      $("#education:last").append(HTMLonlineTitle.replace("%data%", course.title));
-      $("#education:last").append(HTMLonlineSchool.replace("%data%", course.school));
-      $("#education:last").append(HTMLonlineDates.replace("%data%", course.dates));
-      $("#education:last").append(HTMLonlineURL.replace("%data%", course.url));
+        education.onlineCourses.forEach(function(course) {
+                $("#education:last").append(HTMLonlineTitle.replace("%data%", course.title));
+                $("#education:last").append(HTMLonlineSchool.replace("%data%", course.school));
+                $("#education:last").append(HTMLonlineDates.replace("%data%", course.dates));
+                $("#education:last").append(HTMLonlineURL.replace("%data%", course.url));
 
+            }
+
+        )
     }
-
-  )
-}
 }
 education.display();
 
