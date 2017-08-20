a2a6da5a776a5ab27316b0857fb245b3ad102bf8
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2017-08-20 10:39:01 -0700

    post linting

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,23 +1,24 @@
 // Helper functions
-var hasContent = function(object) {
-    // Determine if an object is empty. 
+var hasContent = function (object) {
+    // Determine if an object is empty.
     if (object.length > 0) {
-        return true
+        return true;
     }
-    return false
-}
+    return false;
+};
 
 //work
-// work contains an array of jobs. Each object in the jobs array should contain 
+// work contains an array of jobs. Each object in the jobs array should contain
 // an employer, title, location, dates worked and description strings.
 
 var work = {
-    "jobs": [{
+    "jobs": [
+        {
             "employer": "Activision (Demonware) - Monitoring Team.",
             "title": "Service Reliability Engineer",
             "location": "Vancouver BC",
             "dates": "March 2017-present.",
-            "description": "Building tooling, process, and teams to automate modern monitoring services. Developed new tooling, proposed and implemented process that has directly contributed to a 10x decrease in the number of actionable alerts. System design, and peer review of others system design and architecture. Lead product manager on a new observability platform based around data driven automation.",
+            "description": "Building tooling, process, and teams to automate modern monitoring services. Developed new tooling, proposed and implemented process that has directly contributed to a 10x decrease in the number of actionable alerts. System design, and peer review of others system design and architecture. Lead product manager on a new observability platform based around data driven automation."
         },
         {
             "employer": "Activision (Demonware) - NOC",
@@ -43,13 +44,13 @@ var work = {
     ]
 };
 
-work.display = function() {
+work.display = function () {
     // Display only if jobs exist
     if (hasContent(work.jobs)) {
         // Generate Job Section:
-        $("#workExperience").append('<h2> Job History </h2><hr>');
-        $("#workExperience").append('<div id="jobs-flexbox">');
-        work.jobs.forEach(function(job) {
+        $("#workExperience").append("<h2> Job History </h2><hr>");
+        $("#workExperience").append("<div id='jobs-flexbox'>");
+        work.jobs.forEach(function (job) {
             // Generate job compnent strings
             var employer = HTMLworkEmployer.replace("%data%", job.employer);
             var title = HTMLworkTitle.replace("%data%", job.title);
@@ -57,26 +58,26 @@ work.display = function() {
             var location = HTMLworkLocation.replace("%data%", job.location);
             var description = HTMLworkDescription.replace("%data%", job.description);
             // Create Job Card
-        $("#jobs-flexbox:last").append("<div class='job-item'><div class='job-item-header'>" + title + employer + dates + location + "</div>" + description + "</div>")
-        })
-     //  $("#workExperience").append('</div>');
-    };
-}
-work.display()
+        $("#jobs-flexbox:last").append("<div class='job-item'><div class='job-item-header'>" + title + employer + dates + location + "</div>" + description + "</div>");
+        });
+    }
+};
+work.display();
 
 //projects
-// projects contains an array of projects. Each object in the projects array 
+// projects contains an array of projects. Each object in the projects array
 // should contain title, dates and description strings, and an images array with
 // URL strings for project images.
 
 var projects = {
-    "projects": [{
+    "projects": [
+        {
             "title": "Product managing the monitoring modernisation project for Demonware/Activision online titles",
             "dates": "Feb 2016 to present.",
             "description": "Managing the transition from 7000+ hosts on Nagios with  Tier1 NOC technicians performing triage  to automated monitoring  pipelines based around Sensu and Stackstorm automation.Implemented Agile process, developed and delivered team upskilling and training, actively contributed code, and driving formal requirements gathering.",
             "images": [
                 "images/demonware_logo1.png"
-            ]
+        ]
 
         },
         {
@@ -91,41 +92,41 @@ var projects = {
             "dates": "July 2009-May 2014",
             "description": "Including writing software to federate 8 monitoring service dashboards into a single pane of glass.",
             "images": [
-                "images/demonware_logo1.png",
-            ]
+                "images/demonware_logo1.png"
+        ]
 
         }
     ]
 };
 
-projects.display = function() {
+projects.display = function () {
     // Display projects if they exist.
     if (hasContent(projects.projects)) {
         // Generate project Section:
-        $("#projects:first").append('<h2> Recent Projects </h2><hr>')
-        projects.projects.forEach(function(project) {
+        $("#projects:first").append("<h2> Recent Projects </h2><hr>");
+        projects.projects.forEach(function (project) {
             // Generate project div component strings:
            var title = HTMLprojectTitle.replace("%data%", project.title);
            var dates = HTMLprojectDates.replace("%data%", project.dates);
            var description = HTMLprojectDescription.replace("%data%", project.description);
 
-            // Support n project images. 
-            var projectImages = '';
-            project.images.forEach(function(imageUrl) {
+            // Support n project images.
+            var projectImages = "";
+            project.images.forEach(function (imageUrl) {
                 projectImages = projectImages + HTMLprojectImage.replace("%data%", imageUrl);
             });
             // Create a project.
             $("#projects:last").append("<div class='project-item'>" + projectImages + title + description + dates + "</div>");
-        })
+        });
     }
-}
-projects.display()
+};
+projects.display();
 
 
 // bio
-// bio contains name, role, welcomeMessage, and biopic strings, contacts object 
-// and skills array of skill strings. The contacts object should contain a 
-// mobile number, email address, github username, twitter handle and location. 
+// bio contains name, role, welcomeMessage, and biopic strings, contacts object
+// and skills array of skill strings. The contacts object should contain a
+// mobile number, email address, github username, twitter handle and location.
 // The twitter property is optional.
 
 var bio = {
@@ -140,20 +141,21 @@ var bio = {
         "github": "phoefgen",
         "location": "Vancouver, BC"
     },
-    "skills": ["Python",
-        "Flask",
-        "Sensu",
-        "Nagios",
-        "Stackstorm",
-        "Docker",
-        "Product Mangagement",
-        "Linux",
-        "Process Automation",
-        "System Design"
-    ],
+    "skills":
+        ["Python",
+            "Flask",
+            "Sensu",
+            "Nagios",
+            "Stackstorm",
+            "Docker",
+            "Product Mangagement",
+            "Linux",
+            "Process Automation",
+            "System Design"
+        ]
 };
 
-bio.display = function() {
+bio.display = function () {
     // Display static data:
     $("#headShot").append(HTMLbioPic.replace("%data%", bio.bioPic));
     $("#headLine:last").append(HTMLheaderName.replace("%data%", bio.name));
@@ -169,40 +171,45 @@ bio.display = function() {
     if (hasContent(bio.skills)) {
         $("#glanceSkills:last").append(HTMLskillsStart);
         $("#skills-list:last").append(HTMLskills.replace("%data%", "Skills ["));
-        bio.skills.forEach(function(skill) {
+        bio.skills.forEach(function (skill) {
             $("#skills-list:last").append(HTMLskills.replace("%data%", skill));
-        })
-        $("#skills-list:last").append(HTMLskills.replace("%data%", ']'));
+        });
+        $("#skills-list:last").append(HTMLskills.replace("%data%", "]"));
 
     }
-}
+};
 bio.display();
 
 // education
-// education contains an array of schools. Each object in the schools array 
-// contains name, location, degree dates and url strings, and a majors array of 
+// education contains an array of schools. Each object in the schools array
+// contains name, location, degree dates and url strings, and a majors array of
 // major strings. education also contains an onlineCourses array. Each object in
-// the onlineCourses array should contain a title, school, dates and url 
+// the onlineCourses array should contain a title, school, dates and url
 // strings.
 
 var education = {
-    "schools": [{
-        "name": "Royal Melbourne Institue of Technology",
-        "location": "Melbourne, Australia",
-        "degree": "Computer Science",
-        "majors": ["Handicrafts", "Woodworking"],
-        "dates": "May 6th 2015 - May 12th 2015",
-        "url": "http://rmit.edu.au/"
-    },
-    {
-        "name": "Hogwarts",
-        "location": "England",
-        "degree": "Computer Magic",
-        "majors": ["Reboots", "Virus Spells"],
-        "dates": "May 6th 2015 - May 12th 2015",
-        "url": "http://rmit.edu.au/"
-    }],
-    "onlineCourses": [{
+    "schools":
+        [
+        {
+            "name": "Royal Melbourne Institue of Technology",
+            "location": "Melbourne, Australia",
+            "degree": "Computer Science",
+            "majors": ["Handicrafts", "Woodworking"],
+            "dates": "May 6th 2015 - May 12th 2015",
+            "url": "http://rmit.edu.au/"
+        },
+        {
+            "name": "Hogwarts",
+            "location": "England",
+            "degree": "Computer Magic",
+            "majors": ["Reboots", "Virus Spells"],
+            "dates": "May 6th 2015 - May 12th 2015",
+            "url": "http://rmit.edu.au/"
+        }
+        ],
+    "onlineCourses":
+        [
+        {
             "title": "Full Stack Nanodegree",
             "school": "Udacity",
             "dates": "Febuary 2016 - August 2017",
@@ -215,41 +222,41 @@ var education = {
             "url": "https://udacity.com"
         }
     ]
-}
+};
 
-education.display = function() {
+education.display = function () {
     if (hasContent(education.schools)) {
-        $("#education:first").append('<h2>Education</h2><hr>')
+        $("#education:first").append("<h2>Education</h2><hr>")
 
-        education.schools.forEach(function(school) {
+        education.schools.forEach(function (school) {
             var name = HTMLschoolName.replace("%data%", school.name);
             var date = HTMLschoolDates.replace("%data%", school.dates);
             var location = HTMLschoolLocation.replace("%data%", school.location);
             var degree = HTMLschoolMajor.replace("%data%", school.degree);
-            var minors = ''
+            var minors = "";
             // Fixed API spec, adjusting variable names to correct  semantics.
-            school.majors.forEach(function(major) {
-            minors += major + ' ';
-            })
-            $("#education:last").append("<div class='education-item'><h6>Traditional Facility</h6>" + location + date + name + degree + "<h5>Minors in: " + minors + "</h5>"  + "</div>");
-        })
+            school.majors.forEach(function (major) {
+            minors += major + " ";
+            });
+            $("#education:last").append("<div class='education-item'><h6>Traditional Facility</h6>" + location + date + name + degree + "<h5>Minors in: " + minors + "</h5>" + "</div>");
+        });
     }
-   if (hasContent(education.onlineCourses)) {
-        education.onlineCourses.forEach(function(course) {
+    if (hasContent(education.onlineCourses)) {
+        education.onlineCourses.forEach(function (course) {
             var title = HTMLonlineTitle.replace("%data%", course.title);
             var dates = HTMLonlineDates.replace("%data%", course.dates);
 
             //Construct URL:
             var url = HTMLonlineURL.replace("%data%", course.url);
-            var link = '<a href="%link%"> %school%'.replace('%link%', url);
+            var link = '<a href="%link%"> %school%'.replace("%link%", url);
             var school = HTMLonlineSchool.replace("%data%", course.school);
-            link = link.replace('%school%', school);
+            link = link.replace("%school%", school);
 
 
             $("#education:last").append("<div class='education-item'><h6>Online/Certifications</h6>" + dates + school + title  + link + "</div>");
-        })
+        });
     }
-}
+};
 education.display();
 
 
